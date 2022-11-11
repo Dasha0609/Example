70c07e50b301fd00baaf4e09ef88968997ebbfbe
Dasha <dnazarkina@ya.ru>
2022-11-11 08:27:20 +0300

    added Ex_003 with if/else rule

---
+++ Examples_003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Please enter your name: ");
+String username = Console.ReadLine();
+if (username.ToLower()== "los")
+{
+    Console.WriteLine("GoodMorning, Losyunia");
+}
+else
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
